eac758d2a1eace9fe2c5686af68252836158b2e3
Industrialli Soluções Embarcadas LTDA <137957117+Industrialli@users.noreply.github.com>
2024-08-28 18:17:31 -0300

    Add files via upload

--- industrialli_digital_input.cpp
+++ industrialli_digital_input.cpp
@@ -1,100 +1,249 @@
 #include "digital_input/industrialli_digital_input.h"
 
 void industrialli_digital_input::begin(){
-    gpio_irq_conf[0].GPIOx     = EXTI_01_PE15_GPIO_Port;
-    gpio_irq_conf[0].GPIO_Pin  = EXTI_01_PE15_Pin;
-    gpio_irq_conf[0].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[0].callback  = NULL;
-    gpio_irq_conf[0].irqnb     = EXTI15_10_IRQn;
+    digital_input[A01] = {PB1, NPN, false};
+    digital_input[A02] = {PC5, NPN, false};
+    digital_input[A03] = {PC4, NPN, false};
+    digital_input[A04] = {PA0, NPN, false};
 
-    gpio_irq_conf[1].GPIOx     = EXTI_02_PE14_GPIO_Port;
-    gpio_irq_conf[1].GPIO_Pin  = EXTI_02_PE14_Pin;
-    gpio_irq_conf[1].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[1].callback  = NULL;
-    gpio_irq_conf[1].irqnb     = EXTI15_10_IRQn;
+    digital_input[I01] = {PE15, PNP, false};
+    digital_input[I02] = {PE14, PNP, false};
+    digital_input[I03] = {PE13, PNP, false};
+    digital_input[I04] = {PE12, PNP, false};
+    digital_input[I05] = {PB4,  PNP, false};
+    digital_input[I06] = {PB5,  PNP, false};
+    digital_input[I07] = {PB6,  PNP, false};
+    digital_input[I08] = {PB7,  PNP, false};
 
-    gpio_irq_conf[2].GPIOx     = EXTI_03_PE13_GPIO_Port;
-    gpio_irq_conf[2].GPIO_Pin  = EXTI_03_PE13_Pin;
-    gpio_irq_conf[2].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[2].callback  = NULL;
-    gpio_irq_conf[2].irqnb     = EXTI15_10_IRQn;
+    pinMode(digital_input[A01].port, INPUT);
+    pinMode(digital_input[A02].port, INPUT);
+    pinMode(digital_input[A03].port, INPUT);
+    pinMode(digital_input[A04].port, INPUT);
 
-    gpio_irq_conf[3].GPIOx     = EXTI_04_PE12_GPIO_Port;
-    gpio_irq_conf[3].GPIO_Pin  = EXTI_04_PE12_Pin;
-    gpio_irq_conf[3].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[3].callback  = NULL;
-    gpio_irq_conf[3].irqnb     = EXTI15_10_IRQn;
+    pinMode(digital_input[I01].port, INPUT);
+    pinMode(digital_input[I02].port, INPUT);
+    pinMode(digital_input[I03].port, INPUT);
+    pinMode(digital_input[I04].port, INPUT);
+    pinMode(digital_input[I05].port, INPUT);
+    pinMode(digital_input[I06].port, INPUT);
+    pinMode(digital_input[I07].port, INPUT);
+    pinMode(digital_input[I08].port, INPUT);
 
-    gpio_irq_conf[4].GPIOx     = EXTI_05_PB4_GPIO_Port;
-    gpio_irq_conf[4].GPIO_Pin  = EXTI_05_PB4_Pin;
-    gpio_irq_conf[4].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[4].callback  = NULL;
-    gpio_irq_conf[4].irqnb     = EXTI4_IRQn;
-
-    gpio_irq_conf[5].GPIOx     = EXTI_06_PB5_GPIO_Port;
-    gpio_irq_conf[5].GPIO_Pin  = EXTI_06_PB5_Pin;
-    gpio_irq_conf[5].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[5].callback  = NULL;
-    gpio_irq_conf[5].irqnb     = EXTI9_5_IRQn;
-
-    gpio_irq_conf[6].GPIOx     = EXTI_07_PB6_GPIO_Port;
-    gpio_irq_conf[6].GPIO_Pin  = EXTI_07_PB6_Pin;
-    gpio_irq_conf[6].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[6].callback  = NULL;
-    gpio_irq_conf[6].irqnb     = EXTI9_5_IRQn;
-
-    gpio_irq_conf[7].GPIOx     = EXTI_08_PB7_GPIO_Port;
-    gpio_irq_conf[7].GPIO_Pin  = EXTI_08_PB7_Pin;
-    gpio_irq_conf[7].GPIO_Mode = GPIO_MODE_IT_RISING;
-    gpio_irq_conf[7].callback  = NULL;
-    gpio_irq_conf[7].irqnb     = EXTI9_5_IRQn;
-
-    for (int i = 0; i < 8; i++){
-        leds.set_led(4 + i, LOW);
+    for (int i = 0; i < 12; i++){
+        leds.set_led(i, LOW);
     }
 }
 
-void industrialli_digital_input::attach_interrupt(DIGITAL_INPUT_PIN _pin, void (*_callback)(), uint32_t _mode){
-    gpio_irq_conf[_pin].callback  = _callback;
-    gpio_irq_conf[_pin].GPIO_Mode = _mode;
+void industrialli_digital_input::attach_interrupt(uint8_t _pin, void (*_callback)(), uint32_t _read_mode){
+    digital_input[_pin].read_mode = _read_mode;
+    attachInterrupt(digital_input[_pin].port, _callback, _read_mode);
+}
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin  = gpio_irq_conf[_pin].GPIO_Pin;
-    GPIO_InitStruct.Mode = gpio_irq_conf[_pin].GPIO_Mode;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(gpio_irq_conf[_pin].GPIOx, &GPIO_InitStruct);
+int industrialli_digital_input::read(uint8_t _pin){
+    return digitalRead(digital_input[_pin].port);
+}
 
-    HAL_NVIC_SetPriority(gpio_irq_conf[_pin].irqnb, 1, 1);
-    HAL_NVIC_EnableIRQ(gpio_irq_conf[_pin].irqnb);
+void industrialli_digital_input::enable_analog(uint8_t _pin){
+    digital_input[_pin].analog_as_digital = true;
 }
 
 void industrialli_digital_input::update_leds(){
-    bool led_status[8];
+    int digital_input_status[12];
 
-    led_status[0] = HAL_GPIO_ReadPin(EXTI_01_PE15_GPIO_Port, EXTI_01_PE15_Pin);
-    led_status[1] = HAL_GPIO_ReadPin(EXTI_02_PE14_GPIO_Port, EXTI_02_PE14_Pin);
-    led_status[2] = HAL_GPIO_ReadPin(EXTI_03_PE13_GPIO_Port, EXTI_03_PE13_Pin);
-    led_status[3] = HAL_GPIO_ReadPin(EXTI_04_PE12_GPIO_Port, EXTI_04_PE12_Pin);
-    led_status[4] = HAL_GPIO_ReadPin(EXTI_05_PB4_GPIO_Port, EXTI_05_PB4_Pin);
-    led_status[5] = HAL_GPIO_ReadPin(EXTI_06_PB5_GPIO_Port, EXTI_06_PB5_Pin);
-    led_status[6] = HAL_GPIO_ReadPin(EXTI_07_PB6_GPIO_Port, EXTI_07_PB6_Pin);
-    led_status[7] = HAL_GPIO_ReadPin(EXTI_08_PB7_GPIO_Port, EXTI_08_PB7_Pin);
+    digital_input_status[A01] = digitalRead(digital_input[A01].port);
+    digital_input_status[A02] = digitalRead(digital_input[A02].port);
+    digital_input_status[A03] = digitalRead(digital_input[A03].port);
+    digital_input_status[A04] = digitalRead(digital_input[A04].port);
 
-    for (int i = 0; i < 8; i++){
-        if(gpio_irq_conf[i].GPIO_Mode == GPIO_MODE_IT_RISING){
-            if(led_status[i] == HIGH) {
-                leds.set_led(4 + i, LOW);
-            }else {
-                leds.set_led(4 + i, HIGH);
+    digital_input_status[I01] = digitalRead(digital_input[I01].port);
+    digital_input_status[I02] = digitalRead(digital_input[I02].port);
+    digital_input_status[I03] = digitalRead(digital_input[I03].port);
+    digital_input_status[I04] = digitalRead(digital_input[I04].port);
+    digital_input_status[I05] = digitalRead(digital_input[I05].port);
+    digital_input_status[I06] = digitalRead(digital_input[I06].port);
+    digital_input_status[I07] = digitalRead(digital_input[I07].port);
+    digital_input_status[I08] = digitalRead(digital_input[I08].port);
+
+    for (uint8_t i = 0; i < 12; i++) {
+        if (i < 4) {
+            if (digital_input[i].analog_as_digital) {
+                leds.set_led(i, digital_input_status[i]);
+
             }
-        }
 
-        if(gpio_irq_conf[i].GPIO_Mode == GPIO_MODE_IT_FALLING){
-            if(led_status[i] == HIGH) {
-                leds.set_led(4 + i, HIGH);
-            }else {
-                leds.set_led(4 + i, LOW);
+        } else {
+            if (digital_input[i].read_mode == PNP) {
+                leds.set_led(i, !digital_input_status[i]);
+
+            } else if (digital_input[i].read_mode == NPN) {
+                leds.set_led(i, digital_input_status[i]);
+
             }
         }
     }
 }
+
+// void industrialli_digitalInputsHub::set_all_leds_off(){
+//     for (int i = 0; i < 8; i++){
+//         leds.set_led(4 + i, 0);
+//     }
+// }
+
+// void industrialli_digitalInputsHub::begin_encoder_counting(uint8_t _encoder, bool _sensor_type){
+//     switch (_encoder){
+//         case 0:
+//             sensor_type[0] = _sensor_type;
+//             sensor_type[1] = _sensor_type;
+//             break;
+//         case 1:
+//             sensor_type[2] = _sensor_type;
+//             sensor_type[3] = _sensor_type;
+//             break;
+//         case 2:
+//             sensor_type[4] = _sensor_type;
+//             sensor_type[5] = _sensor_type;
+//             break;
+//         case 3:
+//             sensor_type[6] = _sensor_type;
+//             sensor_type[7] = _sensor_type;
+//             break;
+//     }
+    
+//     begin_interrupt_encoder(_encoder, _sensor_type);
+// }
+
+// void industrialli_digitalInputsHub::begin_interrupt_encoder(uint8_t _encoder, bool _sensor_type){
+//     switch (_encoder){
+//         case 0:
+//             attachInterrupt(digitalPinToInterrupt(EXTI_01), [this, _encoder]() {
+//                 this->count_encoder_interruption(_encoder);
+//             }, FALLING);
+//             break;
+//         case 1:
+//             attachInterrupt(digitalPinToInterrupt(EXTI_03), [this, _encoder]() {
+//                 this->count_encoder_interruption(_encoder);
+//             }, FALLING);
+//             break;
+//         case 2:
+//             attachInterrupt(digitalPinToInterrupt(EXTI_05), [this, _encoder]() {
+//                 this->count_encoder_interruption(_encoder);
+//             }, FALLING);
+//             break;
+//         case 3:
+//             attachInterrupt(digitalPinToInterrupt(EXTI_07), [this, _encoder]() {
+//                 this->count_encoder_interruption(_encoder);
+//             }, FALLING);
+//             break;
+//     }
+
+    
+// }
+
+// void industrialli_digitalInputsHub::count_encoder_interruption(uint8_t _encoder){
+//     noInterrupts();
+
+//     switch(_encoder){
+//         case 0:
+//             encoder_a = digitalRead(EXTI_01);
+//             encoder_b = digitalRead(EXTI_02);
+
+//             if(encoder_last_update[0] - millis() >= 150){
+//                 encoder_velocity[0]    = (60000.0 * abs((int32_t)encoder_last_count[0] - count[0])) / (400.0 * abs((int32_t)encoder_last_update[0] - (int32_t)millis()));
+//                 encoder_last_update[0] = millis();
+//                 encoder_last_count[0]  = count[0];
+//             }
+
+//             if (encoder_a != encoder_b){
+//                 count[0]++;
+//                 encoder_sense[0] = true;
+        
+//             }else{
+//                 count[0]--;
+//                 encoder_sense[0] = false;
+//             }
+//             break;
+//         case 1:
+//             encoder_a = digitalRead(EXTI_03);
+//             encoder_b = digitalRead(EXTI_04);
+
+//             if(encoder_last_update[1] - millis() >= 150){
+//                 encoder_velocity[1]    = (60000.0 * abs((int32_t)encoder_last_count[1] - count[1])) / (400.0 * abs((int32_t)encoder_last_update[1] - (int32_t)millis()));
+//                 encoder_last_update[1] = millis();
+//                 encoder_last_count[1]  = count[1];
+//             }
+
+//             if (encoder_a != encoder_b){
+//                 count[2]++;
+//                 encoder_sense[1] = true;
+        
+//             }else{
+//                 count[2]--;
+//                 encoder_sense[1] = false;
+//             }
+//             break;
+//         case 2:
+//             encoder_a = digitalRead(EXTI_05);
+//             encoder_b = digitalRead(EXTI_06);
+
+//             if(encoder_last_update[2] - millis() >= 150){
+//                 encoder_velocity[2]    = (60000.0 * abs((int32_t)encoder_last_count[2] - count[2])) / (400.0 * abs((int32_t)encoder_last_update[2] - (int32_t)millis()));
+//                 encoder_last_update[2] = millis();
+//                 encoder_last_count[2]  = count[2];
+//             }
+
+//             if (encoder_a != encoder_b){
+//                 count[4]++;
+//                 encoder_sense[2] = true;
+        
+//             }else{
+//                 count[4]--;
+//                 encoder_sense[2] = false;
+//             }
+//             break;
+//         case 3:
+//             encoder_a = digitalRead(EXTI_07);
+//             encoder_b = digitalRead(EXTI_08);
+
+//             if(encoder_last_update[3] - millis() >= 150){
+//                 encoder_last_update[3] = millis();
+//                 encoder_last_count[3]  = count[3];
+//             }
+
+//             if (encoder_a != encoder_b){
+//                 count[6]++;
+//                 encoder_sense[3] = true;
+        
+//             }else{
+//                 count[6]--;
+//                 encoder_sense[3] = false;
+//             }
+//             break;
+//     }
+
+//     interrupts();
+// }
+
+// int industrialli_digitalInputsHub::get_pulses_encoder(uint8_t _encoder){
+//     switch (_encoder){
+//         case 0:
+//             return count[0];
+//             break;
+//         case 1:
+//             return count[2];
+//             break;
+//         case 2:
+//             return count[4];
+//             break;
+//         case 3:
+//             return count[6];
+//             break;
+//     }
+// }
+
+// bool industrialli_digitalInputsHub::get_encoder_sense(uint8_t _encoder){
+//     return encoder_sense[_encoder];
+// }
+
+// double industrialli_digitalInputsHub::get_encoder_velocity(uint8_t _encoder){
+//     return encoder_velocity[_encoder];
+// }
--- industrialli_digital_input.h
+++ industrialli_digital_input.h
@@ -1,36 +1,119 @@
 #ifndef INDUSTRIALLI_DIGITAL_INPUT_H
 #define INDUSTRIALLI_DIGITAL_INPUT_H
 
+#include <Arduino.h>
 #include "leds/industrialli_leds.h"
+
+#define A01 0
+#define A02 1
+#define A03 2
+#define A04 3
+
+#define I01 4
+#define I02 5
+#define I03 6
+#define I04 7
+#define I05 8
+#define I06 9
+#define I07 10
+#define I08 11
+
+#define PNP RISING
+#define NPN FALLING
 
 extern industrialli_leds leds;
 
-enum DIGITAL_INPUT_PIN {
-    I01 = 0,
-    I02 = 1,
-    I03 = 2,
-    I04 = 3,
-    I05 = 4,
-    I06 = 5,
-    I07 = 6,
-    I08 = 7
-};
-
 typedef struct {
-    GPIO_TypeDef *GPIOx;
-    uint16_t GPIO_Pin;
-    uint32_t GPIO_Mode;
-    IRQn_Type irqnb;
-    void (*callback)();
-} gpio_irq_conf_str;
+    uint32_t port;
+    uint8_t read_mode;
+    uint8_t analog_as_digital;
+} digital_input_struct;
 
 class industrialli_digital_input{
 public:
     void begin();
-    void attach_interrupt(DIGITAL_INPUT_PIN _pin, void (*_callback)(), uint32_t _mode);
+    void attach_interrupt(uint8_t _pin, void (*_callback)(), uint32_t _read_mode);
+    int read(uint8_t _pin);
+    void enable_analog(uint8_t _pin);
     void update_leds();
 
-    gpio_irq_conf_str gpio_irq_conf[8];
+private:
+    digital_input_struct digital_input[12];
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #define EXTI_01 PE15
+// #define EXTI_02 PE14
+// #define EXTI_03 PE13
+// #define EXTI_04 PE12
+// #define EXTI_05 PB4
+// #define EXTI_06 PB5
+// #define EXTI_07 PB6
+// #define EXTI_08 PB7
+
+// #define ENCODER_01_02 0
+// #define ENCODER_03_04 1
+// #define ENCODER_05_06 2
+// #define ENCODER_07_08 3
+
+// class industrialli_digitalInputsHub{
+// public:
+//     void begin();
+//     void update_leds();
+//     void set_all_leds_off();
+
+//     void beginUserInterruptInput(uint8_t _pin);
+
+//     void begin_interrupt_input(uint8_t _pin, bool _sensor_type, bool _count_mode);
+//     void begin_digital_input_counting(uint8_t pin, bool _sensor_type, bool _count_mode);   
+//     void count_interrupts(uint8_t _pin, bool _count_mode);
+
+//     int get_count(uint8_t pin);
+//     void clear_count(uint8_t pin);
+
+//     void begin_encoder_counting(uint8_t _encoder, bool _sensor_type);
+//     void begin_interrupt_encoder(uint8_t _encoder, bool _sensor_type);
+//     void count_encoder_interruption(uint8_t _encoder);
+//     int get_pulses_encoder(uint8_t _encoder);
+//     bool get_encoder_sense(uint8_t _encoder);
+//     double get_encoder_velocity(uint8_t _encoder);
+    
+// private:
+//     uint8_t led_status[8];
+//     int input_type[8];
+//     bool sensor_type[8];
+//     volatile int count[8];
+
+//     volatile int encoder_a;
+//     volatile int encoder_b;
+//     volatile bool encoder_sense[4];
+//     volatile double encoder_velocity[4];
+//     volatile uint32_t encoder_last_update[4];
+//     volatile uint32_t encoder_last_count[4];
+// };
 
 #endif
